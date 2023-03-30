ad4f146713ecf3e679560cf01d9b9a4f0b3e4616
mfd2007 <58845044+mfd2007@users.noreply.github.com>
2023-03-30 14:30:59 +0200

    Squashed commit of the following:
    commit a0e24d613f029e3772e414ff1fc4528318fc06a0
    Author: mfd2007 <58845044+mfd2007@users.noreply.github.com>
    Date:   Thu Mar 30 14:30:42 2023 +0200
    
    	Clean up

--- yace/js/lib/productdatabase.js
+++ yace/js/lib/productdatabase.js
@@ -1,7 +1,4 @@
 
-/**
- * 
- */
   function getProductList(){
     return [
     {
@@ -41,18 +38,6 @@
       return {}; 
     }
   }
-
-//  function getProductDetail(id){
-//    switch(id){
-//      case "1":
-//        return {branches:[{category:"product_family", name:"controller", branches: [{category:"product_version", name:"1.0", product:{name:"controller 1.0",product_id:"1"}}]}]};
-//      case "2":
-//        return {branches:[{category:"product_family", name:"controller", branches: [{category:"product_version", name:"2.0", product:{name:"controller 2.0",product_id:"2"}}]}]};
-//      case "3":
-//        return {branches:[{category:"product_family", name:"controller", branches: [{category:"product_version", name:"3.0", product:{name:"controller 3.0",product_id:"3"}}]}]};
-//    }
-//    return [];
-//  }
 
   function getProductFullName(item){
       return item.branches[0].branches[0].product.name;
